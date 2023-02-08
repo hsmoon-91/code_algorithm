803c5dbccfbf1b5afb5dedb3e8c9af9c639480e7
hsmoon-91 <hsmoon9106@gmail.com>
2023-02-08 11:31:12 +0900

    new

---
+++ Queue01.py
@@ -0,0 +1,19 @@
+from collections import deque
+
+# deque 선언
+q = deque()
+print(q)
+# enqueue O(1)
+q.append(1) # [1]
+q.append(2) # [1, 2]
+q.append(3) # [1, 2, 3]
+q.appendleft(0) # [0, 1, 2, 3]
+print(q)
+
+# dequeue O(1)
+q.popleft() # [1, 2, 3]
+q.popleft() # [2, 3]
+print(q)
+q.pop() # [2]
+print(q)
+
