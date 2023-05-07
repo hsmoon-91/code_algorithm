f3ef2e121375ae2947f98daba2257b09b4d41e7c
hsmoon-91 <hsmoon9106@gmail.com>
2023-05-07 10:48:51 +0900

    update

--- algorithm_tct/그리디5.py
+++ algorithm_tct/그리디5.py
@@ -10,4 +10,5 @@ lst = [1,5,4,3,2,4,5,2]
 
 all = sum([x for x in range(n)]) # 전체 리스트에서 두개의 쌍에 대한 조합의 합
 dup = [x for i, x in enumerate(lst) if i != lst.index(x)] # 리스트에서 같은 값 찾기
+
 print(all-len(dup))
