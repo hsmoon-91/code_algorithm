dc06905c332cee2482f4275f576f58957f5176cc
hsmoon-91 <hsmoon9106@gmail.com>
2023-07-27 14:37:12 +0900

    add

---
+++ algorithm_tct/구현1.py
@@ -0,0 +1,17 @@
+
+n = '123406'
+
+length = len(n)
+
+sum1, sum2 = 0, 0
+
+for i in range(length//2):
+    sum1 += int(n[i])
+
+for i in range(length//2, length):
+    sum2 += int(n[i])
+
+if sum1 == sum2:
+    print("LUCKY")
+else:
+    print("READY")
