0289291f15743548587aa392e616e00233fb5df9
hsmoon-91 <hsmoon9106@gmail.com>
2023-08-23 14:28:36 +0900

    update

--- algorithm_all_in_one/LevelOrder.py
+++ algorithm_all_in_one/LevelOrder.py
@@ -11,13 +11,13 @@ def LevelOrder(root) :
     
     while q:
         # 노드 값 추가 : visited
-        cur_node = q.popleft()
-        visited.append(cur_node.value)
+        cur_node = q.popleft() # 방문했다고 판단함.
+        visited.append(cur_node.value) # 노드 값에 값을 추가
         
         # 노드 주소값 추가 : q
-        if cur_node.left:
+        if cur_node.left: # cur_node에 left 주소값이 있다면,
             q.append(cur_node.left)
-        if cur_node.right:
+        if cur_node.right: # cur_node에 right 주소값이 있다면,
             q.append(cur_node.right)
     return visited
 
