f6ff1762169e14eb209e8e1b30fd93cd57ced1a7
hsmoon-91 <hsmoon9106@gmail.com>
2023-08-23 14:21:19 +0900

    update

--- algorithm_all_in_one/LevelOrder.py
+++ algorithm_all_in_one/LevelOrder.py
@@ -10,13 +10,15 @@ def LevelOrder(root) :
     q.append(root)
     
     while q:
+        # 노드 값 추가 : visited
         cur_node = q.popleft()
         visited.append(cur_node.value)
         
+        # 노드 주소값 추가 : q
         if cur_node.left:
             q.append(cur_node.left)
         if cur_node.right:
             q.append(cur_node.right)
     return visited
 
-LevelOrder(root)
+# LevelOrder(root)
