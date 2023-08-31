b8e7b727987629ae5bd5b97f740d29a8c84ef77e
hsmoon-91 <hsmoon9106@gmail.com>
2023-08-31 21:36:21 +0900

    0831update

--- algorithm_all_in_one/LongestConsecutiveSequence.py
+++ algorithm_all_in_one/LongestConsecutiveSequence.py
@@ -1,2 +1,24 @@
-def LongestConsecutiveSewuence():
-    return 
+def LongestConsecutiveSewuence(nums):
+    
+    longest = 0
+    num_dict = {}
+    
+    for num in nums:
+        num_dict[num] = True
+        
+    for num in num_dict:
+        if num-1 not in num_dict :
+            cnt = 1
+            target = num + 1
+            while target in num_dict:
+                cnt += 1
+                target += 1
+            longest = max(longest, cnt)
+        
+    return longest
+
+
+nums = [6,7,100,5,4,4]
+
+answer = LongestConsecutiveSewuence(nums)
+print(answer)
