11748ec4b8fa151aad9e3b597addf745e6588807
hsmoon-91 <hsmoon9106@gmail.com>
2023-08-31 21:27:20 +0900

    update

--- algorithm_all_in_one/KeysandRooms.py
+++ algorithm_all_in_one/KeysandRooms.py
@@ -9,8 +9,7 @@ def keysandRooms(rooms=[]):
 
     def dfs(cur_v = 0):
         
-        visited.append(cur_v)
-        
+        visited.append(cur_v)     
         for next_v in rooms[cur_v]:
             if next_v not in visited:
                 dfs(next_v)            
@@ -18,9 +17,21 @@ def keysandRooms(rooms=[]):
     dfs(0)
     
     
-    
+    def bfs(v):
+        queue = deque()
+        queue.append(v)
+        visited[v] = True
+        while queue:
+            cur_v = queue.popleft()
+            for next_v in rooms[cur_v]:
+                if visited[next_v] == False:
+                    queue.append(next_v)
+                    visited[next_v] = True
+                    
+    bfs(0)
+        
+        
    
 rooms = [[1,3],[2,4],[0],[4],[],[3,4]],
 answer = keysandRooms(rooms) 
 print(answer)
-    
