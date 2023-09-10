a24c5e016bfebd600cb2066448c7a88517077137
hsmoon-91 <hsmoon9106@gmail.com>
2023-09-11 06:43:54 +0900

    0911created

---
+++ sql_test_programmers/pg_groupby07.sql
@@ -0,0 +1,21 @@
+/*자동차 종류 별 특정 옵션이 포함된 자동차 수 구하기*/
+
+-- # 옵션이 한개 이상 포함된 자동차 종류별로 출력
+
+SELECT 
+CAR_TYPE,
+COUNT(*) AS CARS
+FROM CAR_RENTAL_COMPANY_CAR 
+WHERE OPTIONS LIKE '%통풍시트%'
+OR OPTIONS LIKE '%열선시트%'
+OR OPTIONS LIKE '%가죽시트%'
+GROUP BY CAR_TYPE
+ORDER BY CAR_TYPE
+
+
+-- 정규식을 이용한 풀이
+SELECT CAR_TYPE, COUNT(*) AS CARS
+FROM CAR_RENTAL_COMPANY_CAR
+WHERE OPTIONS REGEXP '통풍시트|열선시트|가죽시트'
+GROUP BY CAR_TYPE
+ORDER BY CAR_TYPE;
