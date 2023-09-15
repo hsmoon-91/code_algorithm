f8fc4cf7b5c30321e7120612f6f3322fe00d59ad
hsmoon-91 <hsmoon9106@gmail.com>
2023-09-15 09:32:00 +0900

    0915update

--- sql_test_programmers/pg_sd03.sql
+++ sql_test_programmers/pg_sd03.sql
@@ -1,1 +1,13 @@
 /*조건에 부합하는 중고거래 상태 조회하기*/
+
+-- # 2022년 10월 5일에 등록된 중고거래 게시물 
+-- # 거래상태가 SALES 판매중, RESERVED 예약중 DONE 거래완료
+-- # 서브쿼리 안만들어두 됨.. 
+SELECT
+BOARD_ID, WRITER_ID, TITLE, PRICE, 
+CASE WHEN STATUS = 'SALE' THEN '판매중'
+WHEN STATUS = 'RESERVED' THEN '예약중' 
+WHEN STATUS = 'DONE' THEN '거래완료' ELSE STATUS END AS STATUS
+FROM USED_GOODS_BOARD
+WHERE CREATED_DATE = '2022-10-05'
+ORDER BY 1 DESC
